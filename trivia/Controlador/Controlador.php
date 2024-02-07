242736edb0059d97bc0bf59e6e92495f09598308
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-02-07 09:55:44 -0300

    Codigo funcionando sem session

--- trivia/Controlador/Controlador.php
+++ trivia/Controlador/Controlador.php
@@ -4,25 +4,25 @@ require_once "Classes/RequisitorCurl.php";
 
 class Controlador
 {
-    public static function jogar(string $token_usuario)
+    public static function jogar()
     {
         $perguntas = RequisitorCurl::get_api();
-        $jogo=[];
+        $jogo = [];
         for ($i = 0; $i < 5; $i++) {
-            $pergunta    = $perguntas["results"][$i];
-            $tipo        = $pergunta["type"];
+            $pergunta = $perguntas["results"][$i];
+            $tipo = $pergunta["type"];
             $dificuldade = $pergunta["difficulty"];
-            $categoria   = $pergunta["category"];
-            $questao     = $pergunta["question"];
-            $correta     = $pergunta["correct_answer"];
+            $categoria = $pergunta["category"];
+            $questao = $pergunta["question"];
+            $correta = $pergunta["correct_answer"];
             if ($tipo == "multiple") {
-                $erradas = implode(", ", $pergunta["incorrect_answers"]); 
+                $erradas = implode(", ", $pergunta["incorrect_answers"]);
             } else {
                 $erradas = $pergunta["incorrect_answers"][0];
             }
-            $jogo[$i]=Pergunta::cadastrar_pergunta($tipo, $dificuldade, $categoria, $questao, $correta, $erradas);
+            $jogo[$i] = Pergunta::cadastrar_pergunta($tipo, $dificuldade, $categoria, $questao, $correta, $erradas);
         }
-        return $jogo=Jogo::cadastrar_jogo($jogo[0]->getId(), $jogo[1]->getId(), $jogo[2]->getId(), $jogo[3]->getId(), $jogo[4]->getId(), $token_usuario);
+        return $jogo = Jogo::cadastrar_jogo($jogo[0]->getId(), $jogo[1]->getId(), $jogo[2]->getId(), $jogo[3]->getId(), $jogo[4]->getId());
     }
 
     public static function get_token()
@@ -31,5 +31,3 @@ class Controlador
         return $resultados['token'];
     }
 }
-
-
